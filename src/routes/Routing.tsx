59872cd9516dacf393f535c3cd8360c983ddef52
DadonStyle <noamoni9@gmail.com>
2025-01-24 22:31:32 +0200

    remove unused code from template

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Feed from '../pages/Feed';
-import Login from '../pages/Auth/Login';
-import Register from '../pages/Auth/Register';
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Feed />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
       <Route path="*" element={<Feed />} />
     </Routes>
   );
