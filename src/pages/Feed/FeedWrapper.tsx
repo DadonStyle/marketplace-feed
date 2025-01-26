4fa50d5186acc8e91f6defde94335c604f569f8f
DadonStyle <noamoni9@gmail.com>
2025-01-26 11:51:22 +0200

    stylefix empty states

--- src/pages/Feed/FeedWrapper.tsx
+++ src/pages/Feed/FeedWrapper.tsx
@@ -4,6 +4,7 @@ import { PostObject } from '../../types/types';
 import usePostsFeed from '../../api/usePostsFeed';
 import { Typography } from '@mui/material';
 import usePostViewTracking from '../../hooks/usePostTrack';
+import FlexBox from '../../components/BuildingBlocks/FlexBox/FlexBox';
 
 const ITEMS_PER_PAGE = 6;
 
@@ -23,12 +24,20 @@ const FeedWrapper = () => {
     }
   }, [data]);
 
-  if (isLoading && posts.length === 0) {
-    return <Typography variant="h2">Loading</Typography>;
+  if (isLoading && posts.length !== 0) {
+    return (
+      <FlexBox width="100%" justifyContent="center" alignItems="center" padding={5}>
+        <Typography variant="h2">Loading</Typography>
+      </FlexBox>
+    );
   }
 
-  if (!isLoading && posts.length === 0) {
-    return <Typography variant="h2">No posts available</Typography>;
+  if (!isLoading && posts.length !== 0) {
+    return (
+      <FlexBox width="100%" justifyContent="center" alignItems="center" paddingTop={5}>
+        <Typography variant="h2">No posts available</Typography>
+      </FlexBox>
+    );
   }
 
   return <FeedPage nextPosts={posts} hasMore={data?.hasMore || false} handleNextRequest={handleNextRequest} />;
